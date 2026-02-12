0dad2a54214e9efdbfa1e20ce360de8a18687eac
WhiteDuk3 <i.mahmudov@newuu.uz>
2026-02-13 01:01:15 +0500

    Refactor Django settings for improved structure

--- myproject/settings.py
+++ myproject/settings.py
@@ -94,7 +94,7 @@ USE_TZ = True
 
 # Static files
 STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR / 'static'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
@@ -120,4 +120,5 @@ SECURE_SSL_REDIRECT = True
 SECURE_HSTS_SECONDS = 31536000
 SECURE_HSTS_INCLUDE_SUBDOMAINS = True
 SECURE_HSTS_PRELOAD = True
+
 
