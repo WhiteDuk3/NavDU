9b3f87fd47b9a9fa322caeb4e619b87e88cc1437
WhiteDuk3 <i.mahmudov@newuu.uz>
2026-02-13 11:26:45 +0500

    Add create_admin_once view to urls.py
    Added a temporary view to create an admin user.

--- myproject/urls.py
+++ myproject/urls.py
@@ -4,7 +4,23 @@ from django.conf.urls.static import static
 from django.conf import settings
 from myapp.views import IndexView, ArxivView , AboutView,MuallifView,TahririyatView,TalablarView, email_handler
 
+# TEMPORARY - create admin (only use once, then remove)
+from django.contrib.auth import get_user_model
+from django.http import HttpResponse
+
+def create_admin_once(request):
+    User = get_user_model()
+    username = "NewAdmin"                     # CHANGE THIS
+    email = "isfanchik1212@gmail.com"                  # CHANGE THIS
+    password = "1sf@n20Ob"       # CHANGE THIS to a strong password
+    if not User.objects.filter(username=username).exists():
+        User.objects.create_superuser(username=username, email=email, password=password)
+        return HttpResponse("Admin created")
+    return HttpResponse("Admin already exists")
+
+
 urlpatterns = [
+    path('create-admin-once/', create_admin_once),
     path('admin/', admin.site.urls),
     path('', IndexView.as_view(), name='index'),
     path('arxiv/', ArxivView.as_view(), name='arxiv'),
@@ -15,3 +31,4 @@ urlpatterns = [
     path('email_handler/', email_handler)
     
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
