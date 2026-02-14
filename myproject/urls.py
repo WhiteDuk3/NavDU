6f393fd8eaa5c043de5a1717f645f20fb981cbf4
WhiteDuk3 <i.mahmudov@newuu.uz>
2026-02-14 22:55:54 +0500

    Reorganize urlpatterns in urls.py

--- myproject/urls.py
+++ myproject/urls.py
@@ -1,13 +1,11 @@
 from django.contrib import admin
 from django.urls import path, re_path
-from myapp.views import create_test_file
 from myapp.views import (
     IndexView, ArxivView, AboutView, MuallifView,
     TahririyatView, TalablarView, email_handler, serve_media
 )
 
 urlpatterns = [
-    path('create-test/', views.create_test_file, name='create_test'),
     path('admin/', admin.site.urls),
     path('', IndexView.as_view(), name='index'),
     path('arxiv/', ArxivView.as_view(), name='arxiv'),
@@ -18,5 +16,6 @@ urlpatterns = [
     path('email_handler/', email_handler, name='email_handler'),
     re_path(r'^media/(?P<file_path>.*)$', serve_media, name='serve_media'),
 ]
+
 
 
