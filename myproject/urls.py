782436bce459658cdda3c7e6349c4d3429d08b89
WhiteDuk3 <i.mahmudov@newuu.uz>
2026-02-14 22:27:53 +0500

    Refactor URL patterns and remove unused imports

--- myproject/urls.py
+++ myproject/urls.py
@@ -1,25 +1,11 @@
 from django.contrib import admin
-from django.urls import path , include, re_path
-from django.conf.urls.static import static
-from django.conf import settings
-from myapp.views import IndexView, ArxivView , AboutView,MuallifView,TahririyatView,TalablarView, email_handler
-from myapp import views   # replace 'your_app' with the actual app name
-
-
-
-
-# TEMPORARY - create admin (only use once, then remove)
-from django.contrib.auth import get_user_model
-from django.http import HttpResponse
-
-
-
+from django.urls import path, re_path
+from myapp.views import (
+    IndexView, ArxivView, AboutView, MuallifView,
+    TahririyatView, TalablarView, email_handler, serve_media
+)
 
 urlpatterns = [
-    path('media-status/', views.media_status, name='media_status'),
-    path('debug/media/', views.list_media, name='list_media'),
-    path('debug/', views.debug_db, name='debug_db'),
-    path('list-media/', views.list_media, name='list_media'),
     path('admin/', admin.site.urls),
     path('', IndexView.as_view(), name='index'),
     path('arxiv/', ArxivView.as_view(), name='arxiv'),
@@ -27,18 +13,6 @@ urlpatterns = [
     path('muallif/', MuallifView.as_view(), name='muallif'),
     path('tahririyat/', TahririyatView.as_view(), name='tahririryat'),
     path('talablar/', TalablarView.as_view(), name='talablar'),
-    path('email_handler/', email_handler),
-    re_path(r'^media/(?P<file_path>.*)$', views.serve_media, name='serve_media'),
-    
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-
-
-
-
-
-
-
-
-
-
-
+    path('email_handler/', email_handler, name='email_handler'),
+    re_path(r'^media/(?P<file_path>.*)$', serve_media, name='serve_media'),
+]
