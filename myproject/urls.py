0c43f64fd97fdb924aae793310841f96742041fa
WhiteDuk3 <i.mahmudov@newuu.uz>
2026-02-14 16:57:28 +0500

    Refactor URL patterns and remove admin creation function
    Removed the create_admin_once function and its URL path. Added list_media path to urlpatterns.

--- myproject/urls.py
+++ myproject/urls.py
@@ -12,21 +12,12 @@ from myapp import views   # replace 'your_app' with the actual app name
 from django.contrib.auth import get_user_model
 from django.http import HttpResponse
 
-def create_admin_once(request):
-    User = get_user_model()
-    username = "username"                     # CHANGE THIS
-    email = "isfanchik1212@gmail.com"                  # CHANGE THIS
-    password = "password"       # CHANGE THIS to a strong password
-    if not User.objects.filter(username=username).exists():
-        User.objects.create_superuser(username=username, email=email, password=password)
-        return HttpResponse("Admin created")
-    return HttpResponse("Admin already exists")
+
 
 
 urlpatterns = [
     path('debug/', views.debug_db, name='debug_db'),
-    path('create-admin/', views.create_superuser, name='create_admin'),
-    path('create-admin-once/', create_admin_once),
+    path('list-media/', views.list_media, name='list_media'),
     path('admin/', admin.site.urls),
     path('', IndexView.as_view(), name='index'),
     path('arxiv/', ArxivView.as_view(), name='arxiv'),
@@ -37,6 +28,9 @@ urlpatterns = [
     path('email_handler/', email_handler)
     
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+
+
 
 
 
