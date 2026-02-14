dc36dafaef3617f1b3061802a4feecfd5b9d669f
WhiteDuk3 <i.mahmudov@newuu.uz>
2026-02-14 19:39:50 +0500

    Create media subdirectories in MyappConfig
    Add media subdirectory creation on app startup.

--- myapp/apps.py
+++ myapp/apps.py
@@ -1,6 +1,15 @@
+import os
 from django.apps import AppConfig
-
+from django.conf import settings
 
 class MyappConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'myapp'
+
+    def ready(self):
+        # Create media subdirectories on startup
+        media_root = settings.MEDIA_ROOT
+        subdirs = ['newsniasosi', 'pdfs']  # add any upload_to folders you use
+        for sub in subdirs:
+            path = os.path.join(media_root, sub)
+            os.makedirs(path, exist_ok=True)
